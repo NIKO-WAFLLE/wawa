352939c4f96876a53abe4c585977411f7607d937
niko <morcosis.otaku@gmail.com>
2025-06-06 17:22:57 -0700

    Agrega todos los archivos del proyecto

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+    
+    <div class="container">
+
+        <H1>REPORTE POWER BI</H1>
+        <P>VISUALISACION INETERACTIVA DE DATOS GENERADA EN 
+            POWER BI
+        </P>
+            <div class="iframe-container">
+
+      <iframe title="visualizacionDatamartNorthwind" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiYWY4NmU1NmUtMDRhMi00NzljLTlmMTMtOGNlNTBmYWNhZTUyIiwidCI6ImVhZjQyNzU0LWM3NGItNDRjZi05NjE5LTQyNTY4N2Q4NGViZSJ9" frameborder="0" allowFullScreen="true"></iframe>
+     
+    </div>
+    <div>
+       <h1>hola meow vercion 2</h1>
+    </div>
+
+    </div>
+</template>
+
+
